4e7b0bfb872e06717b27167e5ff3ddbc4b87e33f
Sergejs Stecenko <sergejs.stecenko@gmail.com>
2021-02-18 00:57:36 +0200

    add task 2 styling

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ const app = new Vue({
     el: '#root',
     data: {
         heading: "Subscribe to newsletter",
+        successHeading: "Thanks for subscribing!",
         paragraph: "Subscribe to our newsletter and get 10% discount on pineapple glasses.",
+        successParagraph: "You have successfully subscribed to our email listing. Check your email for the discount code.",
         trophyImage: "../static/images/ic_success.svg",
-        isSubmissionSuccessful: 0,
+        isSubmissionSuccessful: 1,
     }
 
 });
@@ -12,5 +14,3 @@ const app = new Vue({
 
 const successHeading = "Thanks for subscribing!";
 const successParagraph = "You have successfully subscribed to our email listing. Check your email for the discount code.";
-
-// don't forget to add .main-success class to main in Task 2
